8ceab18df05f811401e2f32379df25434c031d31
brandonlehmsnn <brandonlehmann@gmail.com>
2018-07-16 14:55:17 +0000

    Updated compile.sh with correct path

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@
 # Please see the included LICENSE file for more information.
 
 mkdir {cpp,csharp,js,objc,php,python,java,ruby}
-protoc TurtleToTurtle.proto --proto_path=sources/ --cpp_out=cpp/ --csharp_out=csharp/ --js_out=js/ --objc_out=objc/ --php_out=php/ --python_out=python/ --java_out=java/ --ruby_out=ruby/
+protoc sources/TurtleToTurtle.proto --proto_path=sources/ --cpp_out=cpp/ --csharp_out=csharp/ --js_out=js/ --objc_out=objc/ --php_out=php/ --python_out=python/ --java_out=java/ --ruby_out=ruby/
